85100b12e9951d07b4fa96c5e91248d01334eceb
Onns <onns@onns.xyz>
2021-06-02 10:03:54 +0800

    Add test-label api

--- server.go
+++ server.go
@@ -59,6 +59,12 @@ type SaveResult struct {
 	Msg  string `json:"message"`
 }
 
+type TestResult struct {
+	Code int8   `json:"code"`
+	Msg  string `json:"message"`
+	Url  string `json:"url"`
+}
+
 func loadConfig() {
 	filename := "config.json"
 	if _, err := os.Stat(filename); err == nil {
@@ -207,6 +213,86 @@ func getLabelList(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func testLabel(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("method:", r.Method) //获取请求的方法
+	if r.Method == "POST" {
+		// fmt.Println(r.Form["url_long"])
+		r.ParseForm()
+		// for k, v := range r.Form {
+		// 	fmt.Println(k, strings.Join(v, ""))
+		// }
+
+		// r.ParseForm()
+		// fmt.Println("filename:", r.Form["filename"])
+		// fmt.Println("begintime:", r.Form["begintime"])
+
+		// timeUnixNano:=time.Now().UnixNano()
+		begintime, _ := strconv.ParseFloat(r.Form["begintime"][0], 64)
+		endtime, _ := strconv.ParseFloat(r.Form["endtime"][0], 64)
+		tmpquality, _ := strconv.ParseInt(r.Form["quality"][0], 10, 8)
+		quality := int8(tmpquality)
+
+		l := getLabelListFromFile()
+
+		createtime := r.Form["createtime"][0]
+		if createtime == "" {
+			createtime = time.Unix(time.Now().Unix(), 0).Format("2006-01-02-150405")
+		} else {
+			l = deleteLabelByCreateTime(l, createtime)
+		}
+
+		modifytime := time.Unix(time.Now().Unix(), 0).Format("2006-01-02-150405")
+		tmpLabel := VideoLabel{
+			Filename:   r.Form["filename"][0],
+			Starttime:  begintime,
+			Endtime:    endtime,
+			Label:      r.Form["label"][0],
+			Info:       r.Form["info"][0],
+			Quality:    quality,
+			Tip:        r.Form["tip"][0] + " 识别结果：log/" + modifytime + ".txt",
+			Createtime: createtime,
+			Modifytime: modifytime,
+		}
+		// 如果创建时间一样，那证明是同一个标签，应该先删除，再替换
+
+		// else if os.IsNotExist(err) {
+		// path/to/whatever does *not* exist
+
+		// } else {
+		// Schrodinger: file may or may not exist. See err for details.
+
+		// Therefore, do *NOT* use !os.IsNotExist(err) to test for file existence
+
+		// }
+
+		// j := string(b)
+
+		// 如果标签、开始时间、结束时间相同，则是同一标签，应该忽略请求
+		// 这种情况基本不会发生，不浪费时间了
+		l = append(l, tmpLabel)
+		saveLabelListToFile(l, r.Form["filename"][0]+".json")
+
+		b := "filename: " + r.Form["filename"][0] + "\n" + "begintime: " + r.Form["begintime"][0] + "\n" + "endtime: " + r.Form["endtime"][0] + "\n"
+		err := ioutil.WriteFile("static/temp/"+modifytime+".txt", []byte(b), 0644)
+		if err != nil {
+			fmt.Print(err)
+		}
+
+		p := TestResult{
+			Code: 0,
+			Msg:  "标签保存成功",
+			Url:  "log/" + modifytime + ".txt",
+		}
+		w.Header().Set("Content-Type", "application/json")
+
+		if err := json.NewEncoder(w).Encode(&p); err != nil {
+			panic(err)
+		}
+	}
+
+}
+
 func saveLabel(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println("method:", r.Method) //获取请求的方法
@@ -231,12 +317,12 @@ func saveLabel(w http.ResponseWriter, r *http.Request) {
 
 		createtime := r.Form["createtime"][0]
 		if createtime == "" {
-			createtime = time.Unix(time.Now().Unix(), 0).Format("2006-01-02 15:04:05")
+			createtime = time.Unix(time.Now().Unix(), 0).Format("2006-01-02-150405")
 		} else {
 			l = deleteLabelByCreateTime(l, createtime)
 		}
 
-		modifytime := time.Unix(time.Now().Unix(), 0).Format("2006-01-02 15:04:05")
+		modifytime := time.Unix(time.Now().Unix(), 0).Format("2006-01-02-150405")
 		tmpLabel := VideoLabel{
 			Filename:   r.Form["filename"][0],
 			Starttime:  begintime,
@@ -279,6 +365,114 @@ func saveLabel(w http.ResponseWriter, r *http.Request) {
 	}
 
 }
+
+// func testLabel(w http.ResponseWriter, r *http.Request) {
+
+// 	fmt.Println("method:", r.Method) //获取请求的方法
+// 	if r.Method == "POST" {
+// 		// fmt.Println(r.Form["url_long"])
+// 		r.ParseForm()
+// 		// for k, v := range r.Form {
+// 		// 	fmt.Println(k, strings.Join(v, ""))
+// 		// }
+
+// 		// r.ParseForm()
+// 		// fmt.Println("filename:", r.Form["filename"])
+// 		// fmt.Println("begintime:", r.Form["begintime"])
+
+// 		// timeUnixNano:=time.Now().UnixNano()
+// 		begintime, _ := strconv.ParseFloat(r.Form["begintime"][0], 64)
+// 		endtime, _ := strconv.ParseFloat(r.Form["endtime"][0], 64)
+// 		tmpquality, _ := strconv.ParseInt(r.Form["quality"][0], 10, 8)
+// 		quality := int8(tmpquality)
+
+// 		l := getLabelListFromFile()
+
+// 		createtime := r.Form["createtime"][0]
+// 		if createtime == "" {
+// 			createtime = time.Unix(time.Now().Unix(), 0).Format("2006-01-02 15:04:05")
+// 		} else {
+// 			l = deleteLabelByCreateTime(l, createtime)
+// 		}
+
+// 		modifytime := time.Unix(time.Now().Unix(), 0).Format("2006-01-02 15:04:05")
+// 		tmpLabel := VideoLabel{
+// 			Filename:   r.Form["filename"][0],
+// 			Starttime:  begintime,
+// 			Endtime:    endtime,
+// 			Label:      r.Form["label"][0],
+// 			Info:       r.Form["info"][0],
+// 			Quality:    quality,
+// 			Tip:        r.Form["tip"][0],
+// 			Createtime: createtime,
+// 			Modifytime: modifytime,
+// 		}
+
+// 		// rootdir := "/mnt/data/onns/Documents/temp"
+
+// 		// cmd := exec.Command("/bin/bash", "-c", "rm /home/hs/Downloads/20bn-something-something-v1/t/*")
+// 		// cmd = exec.Command("/bin/bash", "-c", "ffmpeg -i \"/mnt/data/onns/Documents/temp/" + r.Form["filename"][0] + "\" -ss "+r.Form["begintime"][0] +" -to "+r.Form["endtime"][0] +" -threads 1 -vf \"scale=-1:256,fps=12\" -q:v 0 \"/home/hs/Downloads/20bn-something-something-v1/t/%05d.jpg\"") //不加第一个第二个参数会报错
+
+// 		// //cmd.Stdout = os.Stdout // cmd.Stdout -> stdout  重定向到标准输出，逐行实时打印
+// 		// //cmd.Stderr = os.Stderr // cmd.Stderr -> stderr
+// 		// //也可以重定向文件 cmd.Stderr= fd (文件打开的描述符即可)
+
+// 		// stdout, _ := cmd.StdoutPipe() //创建输出管道
+// 		// defer stdout.Close()
+// 		// if err := cmd.Start(); err != nil {
+// 		// 	log.Fatalf("cmd.Start: %v")
+// 		// }
+
+// 		// fmt.Println(cmd.Args) //查看当前执行命令
+
+// 		// cmdPid := cmd.Process.Pid //查看命令pid
+// 		// fmt.Println(cmdPid)
+
+// 		// result, _ := ioutil.ReadAll(stdout) // 读取输出结果
+// 		// resdata := string(result)
+// 		// fmt.Println(resdata)
+
+// 		// var res int
+// 		// if err := cmd.Wait(); err != nil {
+// 		// 	if ex, ok := err.(*exec.ExitError); ok {
+// 		// 		fmt.Println("cmd exit status")
+// 		// 		res = ex.Sys().(syscall.WaitStatus).ExitStatus() //获取命令执行返回状态，相当于shell: echo $?
+// 		// 	}
+// 		// }
+
+// 		// fmt.Println(res)
+// 		// 如果创建时间一样，那证明是同一个标签，应该先删除，再替换
+
+// 		// else if os.IsNotExist(err) {
+// 		// path/to/whatever does *not* exist
+
+// 		// } else {
+// 		// Schrodinger: file may or may not exist. See err for details.
+
+// 		// Therefore, do *NOT* use !os.IsNotExist(err) to test for file existence
+
+// 		// }
+
+// 		// j := string(b)
+
+// 		// 如果标签、开始时间、结束时间相同，则是同一标签，应该忽略请求
+// 		// 这种情况基本不会发生，不浪费时间了
+// 		l = append(l, tmpLabel)
+// 		saveLabelListToFile(l, r.Form["filename"][0]+".json")
+
+// 		p := SaveResult{
+// 			Code: 0,
+// 			Msg:  "标签保存成功",
+// 		}
+// 		w.Header().Set("Content-Type", "application/json")
+
+// 		if err := json.NewEncoder(w).Encode(&p); err != nil {
+// 			panic(err)
+// 		}
+// 	}
+
+// }
+
 func deleteLabel(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
 		r.ParseForm()
@@ -307,6 +501,7 @@ func main() {
 	http.HandleFunc("/get-time", getTime)
 	http.HandleFunc("/get-label-list", getLabelList)
 	http.HandleFunc("/save-label", saveLabel)
+	http.HandleFunc("/test-label", testLabel)
 	http.HandleFunc("/delete-label", deleteLabel)
 	http.Handle("/static/", http.FileServer(http.Dir("")))
 	// https://darjun.github.io/2020/01/13/goweb/fileserver/
